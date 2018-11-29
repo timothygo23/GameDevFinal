73cd329369636ee03a18abaa6d85fd062946fd35
Patrick Spencer Tan <patrick_spencer_tan@dlsu.edu.ph>
2018-11-29 23:22:25 +0800

    Add files via upload

---
+++ FPSscripts/PlayerShootingController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+    public float Range = 100;
+    public float ShootingDelay = 2.0f;
+    private Camera _camera;
+    private ParticleSystem _particle;
+    private LayerMask _shootableMask;
+    private float _timer;
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _particle = GetComponentInChildren<ParticleSystem>();
+        //Cursor.lockState = CursorLockMode.Locked;
+        _shootableMask = LayerMask.GetMask("Shootable");
+        _timer = 0;
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (Input.GetMouseButton(0) && _timer >= ShootingDelay)
+        {
+            _timer = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit, Range, _shootableMask))
+        {        
+            print("hit " + hit.collider.gameObject);
+            _particle.Play();
+            ZombieHealth health = hit.collider.GetComponent<ZombieHealth>();
+
+            if (health != null)
+            {
+                health.TakeDamage(1);
+                //if(health.Health < 1)
+                //    for(int i = 0; i< 5; i++)
+                //        Destroy(hit.collider);
+            }
+        }
+    }
+}
---
+++ FPSscripts/ZombieHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : MonoBehaviour
+{
+    public float Health = 5;
+    private Animator _animator;
+    public bool isCritter = false;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        if (Health == 1)
+            isCritter = true;
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (Health <= 0)
+        {
+            return;
+        }
+
+        Health -= damage;
+
+        if (Health <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        BoxCollider[] boxColliders = gameObject.GetComponents<BoxCollider>();
+        foreach (BoxCollider bc in boxColliders) bc.enabled = false;
+
+        MeshCollider[] meshColliders = gameObject.GetComponents<MeshCollider>();
+        foreach (MeshCollider mc in meshColliders) mc.enabled = false;
+
+        if (isCritter)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _animator.SetTrigger("Death");
+        }
+        
+     
+    }
+
+}
