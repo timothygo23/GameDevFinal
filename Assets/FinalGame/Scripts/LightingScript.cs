c79d7d29f85761d1a7694d5563db5800b8b35e2a
gab-santi <gabbysantiago96@yahoo.com>
2018-12-01 18:02:16 +0800

    Light dim relative to time (set 300 secs as default)

---
+++ Assets/FinalGame/Scripts/LightingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingScript : MonoBehaviour {
+
+    [SerializeField] private GameObject lights;
+    [SerializeField] private int time;
+
+    private float dimRate;
+
+    public void Start()
+    {
+        dimRate = (float)(1 / (float)time);
+    }
+
+    private void Update()
+    {
+        lightsDim(dimRate);
+    }
+
+    private void lightsDim(float value)
+    {
+
+        foreach (Transform child in transform)
+        {
+            var intensity = child.gameObject.GetComponent<Light>().intensity;
+            child.gameObject.GetComponent<Light>().intensity = Mathf.Clamp(intensity - value * Time.deltaTime, 0, 1);
+        }
+            
+    }
+
+}
