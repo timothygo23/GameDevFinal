05196195e74ce25558625e952aae49e70d351901
Patrick Spencer Tan <patrick_spencer_tan@dlsu.edu.ph>
2018-12-02 15:45:59 +0800

    Add files via upload

--- Assets/FinalGame/Scripts/CameraController.cs
+++ Assets/FinalGame/Scripts/CameraController.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    /*
     [SerializeField] Camera cam;
-    // [SerializeField] float keyPanSpeed;
 
     private const float keyPanSpeed = 150f;
     private const float updownkeyPanSpeed = 100f;
@@ -17,21 +16,34 @@ public class CameraController : MonoBehaviour {
 
     float camSens = 0.15f; 
     private Vector3 lastMouse = new Vector3(255, 255, 255);
+    */
+    public float speedH = 10.0f;
+    public float speedV = 10.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
     // Use this for initialization
     void Start () {
         //lastMouse = new Vector3(0, 0, 0);
         //lastMouse = cam.transform.localPosition;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         lastMouse = Input.mousePosition - lastMouse;
         lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
         lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
         transform.eulerAngles = lastMouse;
         lastMouse = Input.mousePosition;
+        */
+        //transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
 
         // key input
+        /*
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0, keyPanSpeed * Time.deltaTime, 0));
@@ -40,9 +52,8 @@ public class CameraController : MonoBehaviour {
         {
             transform.Rotate(new Vector3(0, -keyPanSpeed * Time.deltaTime, 0));
         }
-
         
-        /*if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Rotate(new Vector3(-updownkeyPanSpeed * Time.deltaTime , 0, 0));
         }
@@ -50,10 +61,15 @@ public class CameraController : MonoBehaviour {
         if (Input.GetKey(KeyCode.S))
         {
             transform.Rotate(new Vector3(updownkeyPanSpeed * Time.deltaTime, 0, 0));
-        }*/
-        
+        }
+        */
 
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -60f, 90f);
 
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
     }
+
 }
