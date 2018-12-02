9fe11eba90c1692884f2974654eba89cb648e659
gab-santi <gabbysantiago96@yahoo.com>
2018-12-02 18:34:58 +0800

    Added flashlight progression

--- Assets/FinalGame/Scripts/LightingScript.cs
+++ Assets/FinalGame/Scripts/LightingScript.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class LightingScript : MonoBehaviour {
 
     [SerializeField] private GameObject lights;
+    [SerializeField] private Light flashlight;
     [SerializeField] private int time;
+    [SerializeField] private int flashlightTriggerTime;
+    [SerializeField] private float minWaitTime;
+    [SerializeField] private float maxWaitTime;
+    [SerializeField] private float flashlightOffTime;
 
     private float dimRate;
+    private float currentTime = 0;
+
+    private Coroutine co;
 
     public void Start()
     {
@@ -16,7 +24,18 @@ public class LightingScript : MonoBehaviour {
 
     private void Update()
     {
+        currentTime += Time.deltaTime;
+
         lightsDim(dimRate);
+
+        if (currentTime >= flashlightTriggerTime && currentTime < flashlightOffTime)
+        {
+            co = StartCoroutine(Flicker());
+        } else if (currentTime >= flashlightOffTime)
+        {
+            StopAllCoroutines();
+            flashlight.enabled = false;
+        }
     }
 
     private void lightsDim(float value)
@@ -28,6 +47,16 @@ public class LightingScript : MonoBehaviour {
             child.gameObject.GetComponent<Light>().intensity = Mathf.Clamp(intensity - value * Time.deltaTime, 0, 1);
         }
             
+    }
+
+    IEnumerator Flicker()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            flashlight.enabled = !flashlight.enabled;
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+        }
     }
 
 }
