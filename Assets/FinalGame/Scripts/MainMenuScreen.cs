a5b5d3c488ca29a40a30379e1535895eb178ed49
Patrick Spencer Tan <patrick_spencer_tan@dlsu.edu.ph>
2018-12-03 02:01:55 +0800

    Add files via upload

---
+++ Assets/FinalGame/Scripts/MainMenuScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScreen : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnStartClicked()
+    {
+        Debug.Log("Start Clicked!");
+        LoadManager.Instance.LoadScene("FinalGameScene");
+    }
+
+
+}
---
+++ Assets/FinalGame/Scripts/StartScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnPlayClicked()
+    {
+        Debug.Log("Play Clicked!");
+        this.Hide();
+        ViewHandler.Instance.Show(ViewNames.MAIN_MENU_SCREEN);
+        
+    }
+
+    public void OnQuitClicked()
+    {
+        Debug.Log("Quit Clicked!");
+        TwoChoiceDialog dialog = (TwoChoiceDialog)DialogBuilder.Create(DialogBuilder.DialogType.CHOICE_DIALOG);
+        dialog.SetMessage("Are you sure you want to quit the game?");
+        dialog.SetConfirmText("YES");
+        dialog.SetCancelText("No");
+        dialog.SetOnConfirmListener(() =>
+        {
+            Debug.Log("About to quit");
+            Application.Quit();
+        });
+    }
+}
